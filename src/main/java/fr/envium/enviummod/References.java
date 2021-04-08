b0ee15a4d9b361566d74dd856c7933786bf6cc6d
Romaindu35 <romain.sai-tanguy@sfr.fr>
2021-04-08 14:57:09 +0200

    Change minecraft version in References.java

--- src/main/java/fr/envium/enviummod/References.java
+++ src/main/java/fr/envium/enviummod/References.java
@@ -5,6 +5,6 @@ public class References {
     public static final String MODID = "enviummod";
     public static final String NAME = "EnviumMod";
     public static final String VERSION = "1.0";
-    public static final String MINECRAFT_VERSION = "[1.15]";
+    public static final String MINECRAFT_VERSION = "[1.16.5]";
 
 }
